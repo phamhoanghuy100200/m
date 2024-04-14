6b28261bd4844b0aee8d9cc095b914d612ba6a80
Huy Pham <huyhoangpham100200@gmail.com>
2024-04-14 16:59:39 +0700

    text

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import AuthContext from '@/context/AuthContext'
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'messenger-clone',
+  description: 'messenger-clone',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AuthContext>
+
+
+          {children}
+        </AuthContext>
+      </body>
+    </html>
+  )
+}
